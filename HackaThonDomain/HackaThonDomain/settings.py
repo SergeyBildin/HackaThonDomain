760629fb193724f0b84856e1dc21208a51d84e30
SergeyBildin <serega.bildin@outlook.com>
2020-06-20 08:43:21 +0500

    added sqlite3

--- HackaThonDomain/HackaThonDomain/settings.py
+++ HackaThonDomain/HackaThonDomain/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, sys
+import posixpath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'HackaThonDomain.urls'
 
+TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,12 +81,14 @@ WSGI_APPLICATION = 'HackaThonDomain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER' : 'postgres',
-        'PASSWORD' : 'zxcvbn10',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'django_db',
+        #'USER' : 'postgres',
+        #'PASSWORD' : 'zxcvbn10',
+        #'HOST' : '127.0.0.1',
+        #'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -107,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
