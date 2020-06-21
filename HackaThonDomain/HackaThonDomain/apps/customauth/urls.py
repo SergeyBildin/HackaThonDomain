1fb4fff4ebbac8e99a3c94a0b52d4e664545e832
SergeyBildin <serega.bildin@outlook.com>
2020-06-21 13:14:10 +0500

    update CRUD

--- HackaThonDomain/HackaThonDomain/apps/customauth/urls.py
+++ HackaThonDomain/HackaThonDomain/apps/customauth/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        #path('',views.main, name = 'welcome'),
+        path('',views.index, name = 'welcome'),
         path('Registration/', views.RegistrationView.as_view(), name = 'Registration'),
-        #path('secondReg/', views.ProfileCreateView.as_view(), name = 'secondReg'),
+        path('SuccessReg/', views.success_reg, name = 'success_reg'),
         path('login/', views.AuthView.as_view(), name = 'login'),
-        #path('logout', views.Logout.as_view(), name = 'logout'),
+        #path('l', views.Logout.as_view(), name = 'logout'),
         
 ]
--- HackaThonDomain/HackaThonDomain/apps/customauth/views.py
+++ HackaThonDomain/HackaThonDomain/apps/customauth/views.py
@@ -6,27 +6,40 @@ from .models import Account
 from .forms import RegisterForm, AuthForm
 from .UAInterface import checkEmail
 
+def index(request):
+    context = {}
+    template = 'index.html'
+    return render(request,template, context)
+
+def success_reg(request):
+    account = request.user
+    if account == 'AnonymousUser':
+        redirect('login')
+    template = 'SuccessReg.html'
+    context = {}
+    return render(request, template,context)
+
 
 class RegistrationView(FormView):
     model = Account
     template_name = 'Registration.html'
     form_class = RegisterForm
     success_message = 'Пользователь успешно создан'    
-    
-    #success_url = '/secondReg/'    
+    success_url = '/SuccessReg/'    
     def form_valid(self,form):
-        if checkEmail(form.email):
-            form.save()        
-            email = self.request.POST['email']
-            password = self.request.POST['password1']        
+        email = self.request.POST['email']
+        password = self.request.POST['password1']  
+        if checkEmail(email):    
+            form.save() 
             user = authenticate(email=email, password=password)
             print('User:', user)
             login(self.request, user)
-            return super(RegistrationView,self).form_valid(form)
+        return super(RegistrationView,self).form_valid(form)
 
 class AuthView(LoginView):
     template_name = 'Authorization.html'
     form_class = AuthForm
-    #success_url =
+    success_url = '/SuccessReg/' 
     def get_success_url(self):
         return self.success_url
+
